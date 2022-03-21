adfd342238a345eda97d9e91922d7db6c0c00811
luthfynur <nurluthfy98@gmail.com>
2022-03-21 13:24:35 +0700

    fix technology tab margin

--- src/components/TechnologyTab/index.js
+++ src/components/TechnologyTab/index.js
@@ -7,14 +7,14 @@ export default function TechnologyTab({ data, id, onClick }) {
             key={data.name}
             value={idx}
             onClick={onClick}
-            className={`w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] xl:h-[80px] xl:w-[80px] rounded-full border-2 border-white ml-[8px] mr-[8px] xl:ml-[0px] xl:mb-[32px]
+            className={`w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] xl:h-[80px] xl:w-[80px] rounded-full border-2 border-white ml-[8px] mr-[8px] xl:ml-[0px]
               ${
                 idx.toString() === id.toString()
                   ? 'bg-white text-[#0B0D17]'
                   : 'bg-transparent text-white'
               }
                 text-[16px] sm:text-[24px] xl:text-[32px] font-bellefair leading-[18.34px] sm:leading-[27.5px] xl:leading-[36.67px] tracking-[1px] 
-                sm:tracking-[1.5px] xl:tracking-[2px] text-center mt-auto mb-auto xl:mt-0 xl:mb-0
+                sm:tracking-[1.5px] xl:tracking-[2px] text-center mt-auto mb-auto xl:mt-0 xl:mb-[32px]
               `}
           >
             {idx + 1}
